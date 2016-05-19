b516bf61b89be6fc487ec7d33fc2aea45a627753
moly94 <felix.mauler@gmail.com>
2016-05-19 10:13:53 +0200

    Pointless Change

--- app/src/main/java/fh/mc/collaborativewriting/LoginActivity.java
+++ app/src/main/java/fh/mc/collaborativewriting/LoginActivity.java
@@ -38,7 +38,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-
+    //This is a pointless comment
     /**
      * Id to identity READ_CONTACTS permission request.
      */
